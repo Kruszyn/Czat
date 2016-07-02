84b2a9eeac85718dc276d7f2c113180921fd4b13
Kamil Korenkiewicz <kkorenkiewicz1@gmail.com>
2016-07-02 11:47:50 +0200

    Initial project version

---
+++ src/Client.java
@@ -0,0 +1,160 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.text.DefaultCaret;
+
+import java.awt.GridBagLayout;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.net.*;
+
+
+public class Client extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel contentPane;
+	
+	private String name;
+	private String address;
+	private int port;
+	private JTextField txtMessage;
+	private JTextArea history;
+	private DefaultCaret caret;
+
+	private DatagramSocket socket;
+	private InetAddress ip;
+	
+	
+	public Client(String name, String address, int port) {
+		setTitle("Chat Client");
+		this.name = name;
+		this.address = address;
+		this.port = port;
+		boolean connect = openConnection(address, port);
+		if(!connect){
+			System.err.println("Connection failed");
+			console("Connectrion failed");
+		}
+		
+		createWindow();
+		console("Nawi¹zuje po³¹czenie z " + address + ": " + port + ", u¿ytkownik: " + name );
+	}
+
+	private boolean openConnection(String address, int port){
+		try {
+			socket = new DatagramSocket();
+			ip = InetAddress.getByName(address);
+		} catch (UnknownHostException | SocketException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+	
+	private String receive(){
+		byte[] data = new byte[1024];
+		DatagramPacket packet = new DatagramPacket(data, data.length);
+		try {
+			socket.receive(packet);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String message = new String(packet.getData());
+		return message;
+	}
+	
+	private void createWindow(){
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(880,550);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{35,815,30,5};
+		gbl_contentPane.rowHeights = new int[]{30,480,40};
+		gbl_contentPane.columnWeights = new double[]{1.0, 1.0};
+		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		history = new JTextArea();
+		history.setFont(new Font("Arial", Font.PLAIN, 12));
+		history.setEditable(false);
+		caret = (DefaultCaret) history.getCaret();
+		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+		JScrollPane scroll = new JScrollPane(history);
+		GridBagConstraints scrollConstraints = new GridBagConstraints();
+		scrollConstraints.insets = new Insets(0, 0, 5, 5);
+		scrollConstraints.fill = GridBagConstraints.BOTH;
+		scrollConstraints.gridx = 1;
+		scrollConstraints.gridy = 1;
+		scrollConstraints.gridwidth = 2;		
+		scrollConstraints.insets = new Insets(10,0,0,0);
+		contentPane.add(scroll, scrollConstraints);
+		
+		txtMessage = new JTextField();
+		txtMessage.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_ENTER){
+					send(txtMessage.getText());
+				}
+			}
+		});
+		txtMessage.setText("\r\n");
+		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
+		gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
+		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMessage.gridx = 1;
+		gbc_txtMessage.gridy = 2;
+		contentPane.add(txtMessage, gbc_txtMessage);
+		txtMessage.setColumns(10);
+		
+		JButton btnSend = new JButton("Send");
+		btnSend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				send(txtMessage.getText());
+			}
+		});
+		GridBagConstraints gbc_btnSend = new GridBagConstraints();
+		gbc_btnSend.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSend.gridx = 2;
+		gbc_btnSend.gridy = 2;
+		contentPane.add(btnSend, gbc_btnSend);
+		
+		setVisible(true);	
+		txtMessage.requestFocus();
+	}
+	
+	public void send(String message){
+		if(message.equals(""))return;
+		message = name + ": " + message; 
+		console(message);
+		//history.setCaretPosition(history.getDocument().getLength()); gdyby nie dzia³a³o automatyczne scrollowanie okna historii
+		txtMessage.setText("");
+	}
+	
+	public void console(String message){
+		history.append(message + System.lineSeparator());
+	}
+	
+}
---
+++ src/Login.java
@@ -0,0 +1,107 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Login extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1793482340514305910L;
+	private JPanel contentPane;
+	private JTextField txtName;
+	private JTextField txtAddress;
+	private JTextField txtPort;
+	private JLabel lblName;
+	private JLabel lblIpAddress;
+	private JLabel lblPort;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+					/// asdfsafrasfa
+				}
+			}
+		});
+	}
+
+	
+	public Login() {
+	
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		
+		setResizable(false);
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(275,176);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtName = new JTextField();
+		txtName.setBounds(86, 11, 155, 20);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+	    lblName = new JLabel("Name:");
+		lblName.setBounds(27, 14, 56, 14);
+		contentPane.add(lblName);
+		
+		txtAddress = new JTextField();
+		txtAddress.setColumns(10);
+		txtAddress.setBounds(86, 44, 155, 20);
+		contentPane.add(txtAddress);
+		
+		lblIpAddress = new JLabel("IP Address:");
+		lblIpAddress.setBounds(27, 47, 61, 14);
+		contentPane.add(lblIpAddress);
+		
+		txtPort = new JTextField();
+		txtPort.setColumns(10);
+		txtPort.setBounds(86, 75, 155, 20);
+		contentPane.add(txtPort);
+		
+		lblPort = new JLabel("Port:");
+		lblPort.setBounds(27, 78, 56, 14);
+		contentPane.add(lblPort);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name = txtName.getText();
+				String address = txtAddress.getText();
+				int port = Integer.parseInt(txtPort.getText());
+				login(name, address, port);
+				
+			}
+		});
+		btnLogin.setBounds(90, 106, 89, 23);
+		contentPane.add(btnLogin);
+	}
+	
+	private void login(String name, String address, int port) {
+		dispose();
+		new Client(name,address,port);
+	}
+}
