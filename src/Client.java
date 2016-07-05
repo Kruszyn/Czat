d620cc35343a2b7f02f7efc72b4881bec099225a
Kamil Korenkiewicz <kkorenkiewicz1@gmail.com>
2016-07-05 17:40:22 +0200

    Rozdzielenie interfejsu i backendu

--- src/Client.java
+++ src/Client.java
@@ -1,63 +1,45 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.border.EmptyBorder;
-import javax.swing.text.DefaultCaret;
-
-import java.awt.GridBagLayout;
-import javax.swing.JTextArea;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import javax.swing.JButton;
-import java.awt.Insets;
-import javax.swing.JTextField;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.io.IOException;
-import java.net.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import javax.swing.JFrame;
 
 
 public class Client extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	
-	private JPanel contentPane;
+	private DatagramSocket socket;
 	
+
 	private String name;
 	private String address;
 	private int port;
 	private InetAddress ip;
-	
-	private JTextField txtMessage;
-	private JTextArea history;
-	private DefaultCaret caret;
-
 	private Thread send;
-	private DatagramSocket socket;
 	
-	
-	public Client(String name, String address, int port) {
-		setTitle("Chat Client");
+	public Client(String name, String address, int port){
 		this.name = name;
 		this.address = address;
 		this.port = port;
-		boolean connect = openConnection(address);
-		if(!connect){
-			System.err.println("Connection failed");
-			console("Connectrion failed");
-		}
-		createWindow();
-		console("Nawi¹zuje po³¹czenie z " + address + ": " + port + ", u¿ytkownik: " + name );
-		String connection = "/c/" + name;
-		send(connection.getBytes());
 	}
-
-	private boolean openConnection(String address){
+	
+	public String getName(){
+		return name;	
+	}
+	
+	public String getAddress(){
+		return address;
+	}
+	
+	public int getPort(){
+		return port;
+	}
+	
+	boolean openConnection(String address){
 		try {
 			socket = new DatagramSocket();
 			ip = InetAddress.getByName(address);
@@ -68,7 +50,7 @@ public class Client extends JFrame {
 		return true;
 	}
 	
-	private void send(final byte[] data){
+	void send(final byte[] data){
 		send = new Thread("send"){
 			public void run(){
 				DatagramPacket packet = new DatagramPacket(data, data.length, ip, port);
@@ -92,88 +74,6 @@ public class Client extends JFrame {
 		}
 		String message = new String(packet.getData());
 		return message;
-	}
-	
-	private void createWindow(){
-		
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-				| UnsupportedLookAndFeelException e) {
-			e.printStackTrace();
-		}
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(880,550);
-		setLocationRelativeTo(null);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{35,815,30,5};
-		gbl_contentPane.rowHeights = new int[]{30,480,40};
-		gbl_contentPane.columnWeights = new double[]{1.0, 1.0};
-		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
-		contentPane.setLayout(gbl_contentPane);
-		
-		history = new JTextArea();
-		history.setFont(new Font("Arial", Font.PLAIN, 12));
-		history.setEditable(false);
-		caret = (DefaultCaret) history.getCaret();
-		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-		JScrollPane scroll = new JScrollPane(history);
-		GridBagConstraints scrollConstraints = new GridBagConstraints();
-		scrollConstraints.insets = new Insets(0, 0, 5, 5);
-		scrollConstraints.fill = GridBagConstraints.BOTH;
-		scrollConstraints.gridx = 1;
-		scrollConstraints.gridy = 1;
-		scrollConstraints.gridwidth = 2;		
-		scrollConstraints.insets = new Insets(10,0,0,0);
-		contentPane.add(scroll, scrollConstraints);
-		
-		txtMessage = new JTextField();
-		txtMessage.addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent e) {
-				if(e.getKeyCode() == KeyEvent.VK_ENTER){
-					send(txtMessage.getText());
-				}
-			}
-		});
-		txtMessage.setText("\r\n");
-		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
-		gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
-		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtMessage.gridx = 1;
-		gbc_txtMessage.gridy = 2;
-		contentPane.add(txtMessage, gbc_txtMessage);
-		txtMessage.setColumns(10);
-		
-		JButton btnSend = new JButton("Send");
-		btnSend.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				send(txtMessage.getText());
-			}
-		});
-		GridBagConstraints gbc_btnSend = new GridBagConstraints();
-		gbc_btnSend.insets = new Insets(0, 0, 0, 5);
-		gbc_btnSend.gridx = 2;
-		gbc_btnSend.gridy = 2;
-		contentPane.add(btnSend, gbc_btnSend);
-		
-		setVisible(true);	
-		txtMessage.requestFocus();
-	}
-	
-	public void send(String message){
-		if(message.equals(""))return;
-		message = name + ": " + message; 
-		console(message);
-		message = "/m/" + message;
-		send(message.getBytes());
-		txtMessage.setText("");
-	}
-	
-	public void console(String message){
-		history.append(message + System.lineSeparator());
 	}
 	
 }
